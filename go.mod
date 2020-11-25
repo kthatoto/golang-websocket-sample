9f22804f9eb0de34da403f8b22f9f0f6a7c7fc61
kthatoto <kthatoto@gmail.com>
2020-11-26 02:40:05 +0900

    Add server script

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kthatoto/golang-websocket-sample
+
+go 1.15
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	http.HandleFunc("/websocket", serveWebsocket)
+	http.ListenAndServe(":8080", nil)
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+var connections map[*websocket.Conn]bool
+
+func serveWebsocket(w http.ResponseWriter, r *http.Request) {
+	conn, _ := upgrader.Upgrade(w, r, nil)
+
+	// Connect and Handle disconnect
+	connections[conn] = true
+	defer func() {
+		if _, ok := connections[conn]; ok {
+			delete(connections, conn)
+		}
+	}()
+
+	// Read message and Send it to all connections
+	for {
+		messageType, message, _ := conn.ReadMessage()
+		for c := range connections {
+			c.WriteMessage(messageType, message)
+		}
+	}
+}
